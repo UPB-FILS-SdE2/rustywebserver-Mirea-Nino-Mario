cdb31040dc7adeab6551c4ca64993d19b27fb0b0
Mirea-Nino-Mario <161381028+Mirea-Nino-Mario@users.noreply.github.com>
2024-06-23 16:58:36 +0000

    Sde5

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,7 @@ async fn main() {
     loop {
         match listener.accept().await {
             Ok((socket, _)) => {
+                println!("server listening on {}", port);
                 let root_folder = root_folder.clone();
                 tokio::spawn(async move {
                     if let Err(e) = handle_client(socket, root_folder).await {
